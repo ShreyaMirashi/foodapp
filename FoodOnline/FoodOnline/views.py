77a8445698432f1aacd3582e26fd5b2220f57602
Shreya Mirashi <shreyamirashi4@gmail.com>
2024-05-29 19:48:23 +0530

    homepage and static directory is created

--- FoodOnline/FoodOnline/views.py
+++ FoodOnline/FoodOnline/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
-def testUser():
-    return HttpResponse("hey")
+def home(request):
+    return render(request, "home.html")
